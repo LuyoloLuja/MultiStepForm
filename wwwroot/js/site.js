5f718dc942f4ed9ad55828c18be33f1f4df765c9
LuyoloLuja <luyolo.mntuyedwa97@gmail.com>
2025-01-19 23:08:02 +0200

    worked on add-ons to change color when checked - still needs debugging

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,156 +1,172 @@
 ﻿$(document).ready(function () {
-  let arcadeValue = 0;
-  let advancedValue = 0;
-  let proValue = 0;
+    let arcadeValue = 0;
+    let advancedValue = 0;
+    let proValue = 0;
 
-  let updateCycleBills = (isYearly, arcade, advanced, pro) => {
-    const arcadeElement = document.querySelector(arcade);
-    const advancedElement = document.querySelector(advanced);
-    const proElement = document.querySelector(pro);
+    let updateCycleBills = (isYearly, arcade, advanced, pro) => {
+        const arcadeElement = document.querySelector(arcade);
+        const advancedElement = document.querySelector(advanced);
+        const proElement = document.querySelector(pro);
 
-    const yearlyTwoMonthFree = document.querySelectorAll(".two-months-free");
-    const billElement = document.querySelectorAll(".monthly-bill");
+        const yearlyTwoMonthFree = document.querySelectorAll(".two-months-free");
+        const billElement = document.querySelectorAll(".monthly-bill");
 
-    if (isYearly) {
-      arcadeValue = 90;
-      advancedValue = 120;
-      proValue = 150;
+        if (isYearly) {
+            arcadeValue = 90;
+            advancedValue = 120;
+            proValue = 150;
 
-      for (let i = 0; i < yearlyTwoMonthFree.length; i++) {
-        yearlyTwoMonthFree[i].classList.remove("hidden");
-        billElement[i].style.display = "block";
-        billElement[i].style.padding = "0";
-      }
-      arcadeElement.innerHTML = `$${arcadeValue}/yr`;
-      advancedElement.innerHTML = `$${advancedValue}/yr`;
-      proElement.innerHTML = `$${proValue}/yr`;
-    } else {
-      arcadeValue = 9;
-      advancedValue = 12;
-      proValue = 15;
+            for (let i = 0; i < yearlyTwoMonthFree.length; i++) {
+                yearlyTwoMonthFree[i].classList.remove("hidden");
+                billElement[i].style.display = "block";
+                billElement[i].style.padding = "0";
+            }
+            arcadeElement.innerHTML = `$${arcadeValue}/yr`;
+            advancedElement.innerHTML = `$${advancedValue}/yr`;
+            proElement.innerHTML = `$${proValue}/yr`;
+        } else {
+            arcadeValue = 9;
+            advancedValue = 12;
+            proValue = 15;
 
-      for (let i = 0; i < yearlyTwoMonthFree.length; i++) {
-        yearlyTwoMonthFree[i].classList.add("hidden");
-      }
-      arcadeElement.innerHTML = `$${arcadeValue}/mo`;
-      advancedElement.innerHTML = `$${advancedValue}/mo`;
-      proElement.innerHTML = `$${proValue}/mo`;
-    }
-  };
-
-  const validateInputsAndDisplayNextTab = (
-    inputs,
-    button,
-    currentSection,
-    currentSecNumber,
-    followingSec,
-    followingSecNumber
-  ) => {
-    $(button).click(function () {
-      let isValid = $(inputs).find("input").valid();
-
-      if (isValid) {
-        let selectedPlanValue = 0;
-        
-        if (currentSection == ".select-plan") {
-          let selectedPlanElement = document.querySelector(
-            'input[name="BillingType.BillType"]:checked'
-          );
-          
-          if (selectedPlanElement != null) {
-            selectedPlanValue = selectedPlanElement.value;
-          }
+            for (let i = 0; i < yearlyTwoMonthFree.length; i++) {
+                yearlyTwoMonthFree[i].classList.add("hidden");
+            }
+            arcadeElement.innerHTML = `$${arcadeValue}/mo`;
+            advancedElement.innerHTML = `$${advancedValue}/mo`;
+            proElement.innerHTML = `$${proValue}/mo`;
         }
+    };
 
-        let sectionToHide = document.querySelector(currentSection);
-        let stepNumberToHide = document.querySelector(currentSecNumber);
+    const validateInputsAndDisplayNextTab = (
+        inputs,
+        button,
+        currentSection,
+        currentSecNumber,
+        followingSec,
+        followingSecNumber
+    ) => {
 
-        let sectionToDisplay = document.querySelector(followingSec);
-        let stepNumberToDisplay = document.querySelector(followingSecNumber);
+        $(button).click(function () {
+            let isValid = $(inputs).find("input").valid();
 
-        sectionToHide.classList.add("hidden");
-        stepNumberToHide.classList.remove("step-number");
+            if (isValid) {
+                let selectedPlanValue = 0;
 
-        sectionToDisplay.classList.remove("hidden");
-        stepNumberToDisplay.classList.add("step-number");
-      }
-    });
-  };
+                if (currentSection == ".select-plan") {
+                    let selectedPlanElement = document.querySelector(
+                        'input[name="BillingType.BillType"]:checked'
+                    );
 
-  const backButton = (button, currentSection, prevSection) => {
-    document.querySelector(button).addEventListener("click", () => {
-      document.querySelector(currentSection).classList.add("hidden");
-      document.querySelector(prevSection).classList.remove("hidden");
-    });
-  };
+                    if (selectedPlanElement != null) {
+                        selectedPlanValue = selectedPlanElement.value;
+                    }
+                }
 
-  // TO DO - check why my labels are not changing on click
-  const checkboxBgOnChange = (checkbox, label) => {
-    let checkboxElement = document.getElementById(checkbox);
-    let labelElement = document.querySelector(label);
+                // TO DO --- debug - check why label is not changing color
+                if (currentSection == ".add-ons") {
+                    checkboxBgOnChange("online-service-checkbox", ".online-service-label");
+                }
 
-    checkboxElement.addEventListener("change", (event) => {
-      let isChecked = event.target.checked;
+                let sectionToHide = document.querySelector(currentSection);
+                let stepNumberToHide = document.querySelector(currentSecNumber);
 
-      console.log(isChecked);
-      if (isChecked) {
-        labelElement.classList.add("custom-label");
-      } else {
-        labelElement.classList.remove("custom-label");
-      }
-      console.log(labelElement);
-    });
+                let sectionToDisplay = document.querySelector(followingSec);
+                let stepNumberToDisplay = document.querySelector(followingSecNumber);
 
-    // Handle clicks on the label itself to simulate a change
-    labelElement.addEventListener("click", (event) => {
-      checkboxElement.checked = !checkboxElement.checked; // Toggle checkbox state manually
-      checkboxElement.dispatchEvent(new Event("change")); // Trigger the 'change' event
-    });
-  };
+                sectionToHide.classList.add("hidden");
+                stepNumberToHide.classList.remove("step-number");
 
-  $("form").validate();
+                sectionToDisplay.classList.remove("hidden");
+                stepNumberToDisplay.classList.add("step-number");
+            }
+        });
+    };
 
-  validateInputsAndDisplayNextTab(
-    ".personal-info",
-    "#validate-personal-info",
-    ".personal-info",
-    ".step-number-1",
-    ".select-plan",
-    ".step-number-2"
-  );
-  validateInputsAndDisplayNextTab(
-    ".select-plan",
-    "#validate-billing-type",
-    ".select-plan",
-    ".step-number-2",
-    ".add-ons",
-    ".step-number-3"
-  );
-  validateInputsAndDisplayNextTab(
-    ".add-ons",
-    "#validate-add-ons",
-    ".add-ons",
-    ".step-number-3",
-    ".finishing-up",
-    ".step-number-4"
-  );
+    const backButton = (button, currentSection, prevSection) => {
+        document.querySelector(button).addEventListener("click", () => {
+            document.querySelector(currentSection).classList.add("hidden");
+            document.querySelector(prevSection).classList.remove("hidden");
+        });
+    };
 
-  updateCycleBills(
-    false,
-    ".arcade-plan-bill",
-    ".advanced-plan-bill",
-    ".pro-plan-bill"
-  );
+    // TO DO - check why my labels are not changing on click
+    const checkboxBgOnChange = (checkbox, label) => {
+        let checkboxElement = document.getElementById(checkbox);
+        let labelElement = document.querySelector(label);
 
-  $("#billing-cycle").change(function () {
-    updateCycleBills(
-      $(this).is(":checked"),
-      ".arcade-plan-bill",
-      ".advanced-plan-bill",
-      ".pro-plan-bill"
+        // console.log(checkboxElement);
+        // console.log(labelElement);
+
+        // if (checkboxElement.checked == true) {
+        //     labelElement.classList.add("custom-label");
+        // } else {
+        //     labelElement.classList.remove("custom-label");
+        // }
+
+        checkboxElement.addEventListener("change", (event) => {
+            let isChecked = event.target.checked;
+
+            console.log("isChecked " + isChecked);
+            if (isChecked) {
+                labelElement.classList.add("custom-label");
+            } else {
+                labelElement.classList.remove("custom-label");
+            }
+            console.log(labelElement);
+        });
+
+        // Handle clicks on the label itself to simulate a change
+        labelElement.addEventListener("click", (event) => {
+            checkboxElement.checked = !checkboxElement.checked; // Toggle checkbox state manually
+            checkboxElement.dispatchEvent(new Event("change")); // Trigger the 'change' event
+        });
+    };
+
+    $("form").validate();
+
+    validateInputsAndDisplayNextTab(
+        ".personal-info",
+        "#validate-personal-info",
+        ".personal-info",
+        ".step-number-1",
+        ".select-plan",
+        ".step-number-2"
     );
-  });
+    validateInputsAndDisplayNextTab(
+        ".select-plan",
+        "#validate-billing-type",
+        ".select-plan",
+        ".step-number-2",
+        ".add-ons",
+        ".step-number-3"
+    );
+    validateInputsAndDisplayNextTab(
+        ".add-ons",
+        "#validate-add-ons",
+        ".add-ons",
+        ".step-number-3",
+        ".finishing-up",
+        ".step-number-4"
+    );
 
-  backButton("#btn-back-bill-type", ".select-plan", ".personal-info");
-  backButton("#btn-back-add-ons", ".add-ons", ".select-plan");
+    updateCycleBills(
+        false,
+        ".arcade-plan-bill",
+        ".advanced-plan-bill",
+        ".pro-plan-bill"
+    );
+
+    $("#billing-cycle").change(function () {
+        updateCycleBills(
+            $(this).is(":checked"),
+            ".arcade-plan-bill",
+            ".advanced-plan-bill",
+            ".pro-plan-bill"
+        );
+    });
+
+    backButton("#btn-back-bill-type", ".select-plan", ".personal-info");
+    backButton("#btn-back-add-ons", ".add-ons", ".select-plan");
+    backButton("#btn-back-finish-up", ".finishing-up", ".add-ons");
 });
